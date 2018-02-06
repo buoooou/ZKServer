af889ee27b6857f3ff539ce345dc56490efccb25
zhangkuo <769329489@qq.com>
2018-02-06 20:29:20 +0800

    新加monitor接口

---
+++ src/main/java/kafeihu/zk/server/monitor/MonitorContext.java
@@ -0,0 +1,25 @@
+package kafeihu.zk.server.monitor;
+
+import kafeihu.zk.server.core.context.ModuleContext;
+
+/**
+ * 监控上下文
+ * Created by zhangkuo on 2016/11/27.
+ */
+public class MonitorContext extends ModuleContext
+{
+
+    public MonitorContext(String mModuleName)
+    {
+        super(mModuleName);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void loadContextData(String path) throws Exception
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
